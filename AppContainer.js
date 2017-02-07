f05e7376127b9489a302f011e33c81fb85aff074
Hiu-Fung Hau <Hiu-Fung@Hiu-Fungs-MacBook-Pro.local>
2017-02-07 09:18:57 +0100

    Add react-native-ibeacon
    - add reactn-native-ibeacon npm package
    - add libRNBeacon & CoreLocation framework to build phases
    - subscription on 'beaconsDidRange' working (i.e. console.logs)

--- AppContainer.js
+++ AppContainer.js
@@ -18,9 +18,52 @@ import {
 	View,
 	StyleSheet,
 	TabBarIOS,
-	NavigatorIOS
+	NavigatorIOS,
+	DeviceEventEmitter
 } from 'react-native';
 
+var Beacons = require('react-native-ibeacon');
+
+// Define a region which can be identifier + uuid,
+// identifier + uuid + major or identifier + uuid + major + minor
+// (minor and major properties are numbers)
+var region = {
+	identifier: 'Estimotes',
+	uuid: '10F86430-1346-11E4-9191-0800200C9A66'
+	// uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D'
+	// e20a39f4-73f54bc4-a12f-17d1ad07a961 From raspberry forum
+};
+
+// Request for authorization while the app is open
+Beacons.requestWhenInUseAuthorization();
+
+Beacons.startMonitoringForRegion(region);
+Beacons.startRangingBeaconsInRegion(region);
+
+Beacons.startUpdatingLocation();
+
+// Listen for beacon changes
+var subscription = DeviceEventEmitter.addListener(
+	'beaconsDidRange',
+	(data) => {
+		console.log('beaconsDidRange');
+		console.log(data.region.identifier);
+		console.log(data.region.uuid);
+
+		// data.region - The current region
+		// data.region.identifier
+		// data.region.uuid
+
+		// data.beacons - Array of all beacons inside a region
+		//  in the following structure:
+		//    .uuid
+		//    .major - The major version of a beacon
+		//    .minor - The minor version of a beacon
+		//    .rssi - Signal strength: RSSI value (between -100 and 0)
+		//    .proximity - Proximity value, can either be "unknown", "far", "near" or "immediate"
+		//    .accuracy - The accuracy of a beacon
+	}
+);
 
 
 var Feed = require('./Feed');
